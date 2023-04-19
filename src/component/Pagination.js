3d6b8e8b8debd2879ddb43b64f7753d830b383f1
Ebrahim Abd EL Razik <ebrahimabdelrazik2002@gmail.com>
2023-04-19 21:53:16 +0200

    a

--- src/component/Pagination.js
+++ src/component/Pagination.js
@@ -1,16 +1,26 @@
 import ReactPaginate from "react-paginate";
-import { getMovies } from "../rtx/Slices/MoviesSlice";
+import { ApiKey, getMovies } from "../rtx/Slices/MoviesSlice";
 import { useDispatch } from "react-redux";
 import "./Pagination.css";
-
+import axios from "axios";
+import { useState } from "react";
 function PaginationComponent() {
+  const [pageCount, setPageCount] = useState(0);
+
+  const getPageCount = async () => {
+    const res = await axios.get(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${ApiKey}&language=en-US`
+    );
+
+    setPageCount(res.data.total_pages);
+  };
+
+  getPageCount();
   const dispatch = useDispatch();
 
   const handlePageClick = (data) => {
     dispatch(getMovies(data.selected + 1));
   };
-
-  const pageCount = 500;
 
   const pagination = (pagination) => {
     return (
